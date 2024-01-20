9fe2d365399fca291685edcc30399abefef9e581
Valery Raraykin <vand_er@mail.ru>
2024-01-20 17:07:22 +0300

    Исправил модели

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Text
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -8,8 +8,10 @@ class Dish(Base):
     __tablename__ = "dishes"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    title = Column(String, unique=True)
+    description = Column(Text)
     price = Column(Float)
+
     submenu_id = Column(Integer, ForeignKey("submenus.id"))
 
     submenu = relationship("Submenu", back_populates="dishes")
@@ -19,7 +21,9 @@ class Submenu(Base):
     __tablename__ = "submenus"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    title = Column(String, unique=True)
+    description = Column(Text)
+
     menu_id = Column(Integer, ForeignKey("menus.id"))
 
     menu = relationship("Menu", back_populates="submenus")
@@ -31,7 +35,8 @@ class Menu(Base):
     __tablename__ = "menus"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    title = Column(String, unique=True)
+    description = Column(Text)
 
     submenus = relationship(
         "Submenu", back_populates="menu", cascade="all, delete-orphan")
